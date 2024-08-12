2ef24d0c16ceee3dd8766ccb9c00a90ea59bc1fb
Lane Wofford <lanedwofford@gmail.com>
2024-08-12 16:10:17 +0000

    reformmated and renamed to include directories for each python class. post-exercises directory was added as well for exercises after book is completed

---
+++ py100/post-exercises/documentation.py
@@ -0,0 +1,28 @@
+# FINDING INDEX
+
+fruits = ["apple", "banana", "cherry", "peach", "watermelon"]
+
+# print(fruits.index('cherry')) 
+
+# OUT OF BOUNDS 
+
+# there will be an index error 
+
+# LARGE NUMBERS - needed hint 
+
+# underscores can be used as they are ignored when determing value of 
+# numeric literal 
+
+# print(1_987_654_321) # these are the same
+# print(19_87_65_4321) 
+
+# ten = 10 
+
+# print(fr"{ten} yodelo \ he") 
+
+# print("hello" "world") # this is probably wrong. doc says something about 
+# a + is needed at runtime???
+
+# CHECKING DATA TYPES 
+
+print(type(23.5))
