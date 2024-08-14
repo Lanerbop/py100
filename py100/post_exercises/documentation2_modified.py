195e8788f9e6ed40225a0689b54cac0191cc3df5
Lane Wofford <lanedwofford@gmail.com>
2024-08-14 20:20:35 +0000

    added documentation 2 post_exercise w variable modification

---
+++ py100/post_exercises/documentation2_modified.py
@@ -0,0 +1,19 @@
+# STRING FORMATTING
+
+# big = "Big"
+# dog = "DAWGGGGGG"
+
+# print("What's up {} {}???".format(big, dog))
+
+# print(f"What's up {big} {dog}???")
+
+# STYLE GUIDE
+
+# ice_cream_density = 10
+
+# while ice_cream_density > 0:
+#     print('Drip...')
+#     ice_cream_density -= 1
+
+# print('The ice cream melted.')
+
